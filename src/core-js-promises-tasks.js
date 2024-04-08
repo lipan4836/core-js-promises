4a557d3284328c9c187514d3a2d3e4938a1cc4e7
Anton <lipan48@gmail.com>
2024-04-08 19:14:17 +0300

    feat: resume task, 5 pass

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -78,8 +78,8 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.any(promises);
 }
 
 /**
@@ -93,8 +93,8 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
